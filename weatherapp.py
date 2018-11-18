87f57115e70ebb974805ce46ea35ddd6d27ddc0c
AndreiPisnia <a.pisnia@gmail.com>
2018-11-18 22:55:10 +0200

    Add conditions. URL changed to english. Can't get condition in ukrainian. Lesson2. Task 4.

--- weatherapp.py
+++ weatherapp.py
@@ -4,7 +4,9 @@
 import html
 from urllib.request import urlopen, Request
 
-ACCU_URL = " https://www.accuweather.com/uk/ua/kyiv/324505/weather-forecast/324505"
+#Change language to english. Сan't get conditions in ukrainian.
+#ACCU_URL = " https://www.accuweather.com/uk/ua/kyiv/324505/weather-forecast/324505"
+ACCU_URL = " https://www.accuweather.com/en/ua/kyiv/324505/weather-forecast/324505?lang=en-us"
 
 # getting page from server
 headers = {'User-Agent': 'Mozila/5.0 (X11; Fedora; Linux x86_64;)'}
@@ -12,6 +14,7 @@ accu_request = Request(ACCU_URL, headers=headers)
 accu_page = urlopen(accu_request).read()
 accu_page = str(accu_page)
 
+# getting temperature
 ACCU_TEMP_TAG = '<span class="large-temp">'
 accu_temp_tag_size = len(ACCU_TEMP_TAG)
 accu_temp_tag_index = accu_page.find(ACCU_TEMP_TAG)
@@ -26,4 +29,19 @@ for char in accu_page[accu_temp_value_start:]:
 print('AccuWeather: \n')
 print(f'Temperature: {html.unescape(accu_temp)} \n')
 
+# getting conditions
+ACCU_COND_TAG = '<span class="cond">'
+accu_cond_tag_size = len(ACCU_COND_TAG)
+accu_cond_tag_index = accu_page.find(ACCU_COND_TAG)
 
+accu_cond_value_start = accu_cond_tag_index + accu_cond_tag_size
+accu_cond = ''
+char_html = ''
+for char in accu_page[accu_cond_value_start:]:
+    if char != '<':
+        char_html = html.unescape(char)
+        accu_cond += char_html
+    else:
+        break
+      
+print(f'Condition: {accu_cond} \n')
