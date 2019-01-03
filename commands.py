0d691f041d00b73dff875590bde8321aa68237c3
AndreiPisnia <a.pisnia@gmail.com>
2019-01-03 23:25:43 +0200

    Add class Configure

---
+++ commands.py
@@ -0,0 +1,30 @@
+"""App commands.
+"""
+
+from abstract import Command
+
+
+class Configurate(Command):
+    """Help to configure weatherapp providers.
+    """
+
+    def get_parser(self):
+        parser = super().get_parser()
+        parser.add_argument('provider', help='Provider name')
+        return parser
+
+    def run(self, argv):
+        parsed_args = self.get_parser().parse_args(argv)
+        if parsed_args.provider:
+            provider_name = parsed_args.provider
+            if provider_name in self.app.providermanager:
+                provider_factory = self.app.providermanager.get(provider_name)
+                provider_factory(self.app).configurate()
+
+
+
+class Providers(Command):
+    """Print list of all providers
+    """
+    pass
+
