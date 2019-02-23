5245006e7eb92feb5e5a9d853b4af6e278b6a79d
AndreiPisnia <a.pisnia@gmail.com>
2019-02-23 16:13:36 +0200

    Add tests for main application file

--- weatherapp/core/tests/unit/test_app.py
+++ weatherapp/core/tests/unit/test_app.py
@@ -11,16 +11,36 @@ class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.parser = App._arg_parse(self)
 
-    def test_arg_parser(self):
+    def test_arg_parse(self):
         """ Test application argument parser creation.
         """    
 
         self.assertIsInstance(self.parser, argparse.ArgumentParser)
         
-        print('test complete')
+        print('test 1 complete')
 
     def test_arg_parser_default_values(self):
+        """Test application argument parser default values.
+        """
 
         parsed_args = self.parser.parse_args([])
         self.assertIsNone(parsed_args.command)
+        self.assertEqual(parsed_args.formatter, 'table')
         self.assertFalse(parsed_args.debug)
+        
+    def test_arg_parser_arg(self):
+        """Test application argument parser.
+        """
+
+        parsed_args = self.parser.parse_args(['accu', '--debug',
+                                              '--refresh', '-v'])
+        self.assertEqual(parsed_args.command, 'accu')
+        self.assertEqual(parsed_args.formatter, 'table')
+        self.assertTrue(parsed_args.debug)
+        self.assertTrue(parsed_args.refresh)    
+        self.assertEqual(parsed_args.verbose_level, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
